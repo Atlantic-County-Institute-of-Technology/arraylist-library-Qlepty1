a49b2bf538bbd1d97edb66e624f3d1cb5f2c3100
Jamie Aleman <100420254+Qlepty1@users.noreply.github.com>
2025-03-10 13:06:48 -0400

    Update Library.java

--- Library.java
+++ Library.java
@@ -2,15 +2,15 @@ import java.util.ArrayList;
 import java.util.Random;
 
 
-public class Library {
+public class Library {                                                // Library class creator. 
     ArrayList<String> collection = new ArrayList<String>();
 
 
-    public void addItem(String item){ collection.add(item); }
-    public void removeItem(String item){ collection.remove(item); }
-    public String getNextItem(){ return collection.size()>0 ? collection.get(0) : "Nothing next gang"; }
-    public void skipNextItem(){ if(collection.size()>0) collection.remove(0); }
-    public void sortAlphabet(){ collection.sort(String::compareTo); }
-    public int find(String item){ return collection.indexOf(item); }
-    public void shuffle(){ collection.sort((a, b)->new Random().nextInt()); }
+    public void addItem(String item){ collection.add(item); }            // add item method
+    public void removeItem(String item){ collection.remove(item); }      // remove item method
+    public String getNextItem(){ return collection.size()>0 ? collection.get(0) : "Nothing next gang"; }  // gets the next item and has error catching if theres nothing left in the queue 
+    public void skipNextItem(){ if(collection.size()>0) collection.remove(0); }        // skips the next item in the queue
+    public void sortAlphabet(){ collection.sort(String::compareTo); }  // sorts the queue in alphabetical order 
+    public int find(String item){ return collection.indexOf(item); }  // find a track
+    public void shuffle(){ collection.sort((a, b)->new Random().nextInt()); } // shuffles the queue
 }
